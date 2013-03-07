1eb081e6dec0b5e04717ed0c24986e695722c054
Srinivas Singanamalla <ssinganamalla@yahoo.com>
2013-03-07 16:24:21 -0500

    test

--- iosproject/iosproject/SQLBIFirstViewController.m
+++ iosproject/iosproject/SQLBIFirstViewController.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	//gxf Do any additional setup after loading the view, typically from a nib.
+	//gxf hjhhjDo any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning
